a833b9d1435bc58bae59feb4dc9c6dcdbc21f392
Andrés Pausín <111588426+andrespausin@users.noreply.github.com>
2024-01-23 12:08:24 -0400

    add: lastChanges

--- compEmergente/discordBot/main.py
+++ compEmergente/discordBot/main.py
@@ -18,36 +18,35 @@ async def on_ready():
 @client.command()
 # Función que se ejecuta cuando se escribe ?hola. El bot responde con un mensaje
 async def hola(ctx):
-    await ctx.send('Hola, soy un bot de prueba. ¿Cómo estás?')
+    await ctx.send('Hola, {} soy un bot de prueba. ¿Cómo estás? {}'.format(ctx.author.name, ctx.author.mention))
     
 @client.command()
 # Función que se ejecuta cuando se escribe ?adios. El bot responde con un mensaje
 async def adios(ctx):
-    await ctx.send('Adiós, espero verte pronto.')
+    await ctx.send('Adiós, {} espero verte pronto. {}'.format(ctx.author.name, ctx.author.mention))
 
 @client.command()    
 # Función que se ejecuta cuando se escribe ?ping. El bot responde con un mensaje
 async def ping(ctx):
-    await ctx.send('Pong!')
-    
+    if(round(client.latency * 1000) > 100):
+        await ctx.send('Tienes una latencia muy alta, deberías checkear tu internet no crack :( {}ms'.format(round(client.latency * 1000)) + ' {}'.format(ctx.author.mention))
+    else:
+        await ctx.send('Tienes una excelente latencia hermano, que crack! {}ms'.format(round(client.latency * 1000)) + ' {}'.format(ctx.author.mention))
+        
 @client.command()
 # Función que se ejecuta cuando se escribe ?info. El bot responde con un mensaje
 async def info(ctx):
-    await ctx.send('Soy un bot de prueba. Estoy en desarrollo.')
+    await ctx.send('Soy un bot de prueba. Estoy en desarrollo. Gracias por usarme de todos modos. :) {}'.format(ctx.author.mention))
     
 @client.command()
 # Función que se ejecuta cuando se escribe ?comandos. El bot responde con un mensaje
 async def comandos(ctx):
     await ctx.send('Los comandos disponibles son: ?hola, ?adios, ?ping, ?info, ?comandos, ?clear')
-    
-@client.command()
-# Función que se ejecuta cuando se escribe ?ayuda. El bot responde con un mensaje
-async def clear(ctx, amount=5):
-    await ctx.channel.purge(limit=amount)
-        
+            
 @client.command()
 # Función que se ejecuta cuando se escribe ?clear. El bot borra mensajes del chat
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
     
-client.run('MTE5NDc5MTc2MTgxMDI5Njg3Mw.G7QxQj.YyhF0Z7Ff8GmnE0P0SYUyjRBU0j0UDU0KeUIyI') 
+#Se ejecuta el código del bot. Se le asigna el token del bot
+client.run('MTE5NDc5MTc2MTgxMDI5Njg3Mw.GbI2jV.3GSmcYPvLo_kHHrEmUgB4_Y8lrYnMEH5wuxfsA') 
